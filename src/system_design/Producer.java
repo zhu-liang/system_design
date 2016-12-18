226010ff5e790b294fa0d6f4bf575fad973647d1
Zhu Liang <zlux2357@gmail.com>
2016-12-18 16:33:44 +0800

    Added something from branch develop
    Change-Id: I980c73d4964eebd38c77bb7aa8c46db521139245

--- src/system_design/Producer.java
+++ src/system_design/Producer.java
@@ -4,6 +4,7 @@ public class Producer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Greets from develop branch");
 		System.out.println("Hi, I am Producer");
 	}
 
