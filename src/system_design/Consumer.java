9f3e2f44e78cb70f59e93d342624fa151db2cb3c
Zhu Liang <julian.liangzhu@gmail.com>
2016-12-18 16:24:22 +0800

    Preparation for ActiveMQ Usage
    Added initial Producer and Consumer class, get ready to be used together
    with ActiveMQ on system.

---
+++ src/system_design/Consumer.java
@@ -0,0 +1,9 @@
+package system_design;
+
+public class Consumer {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, I am Consumer");
+	}
+
+}
---
+++ src/system_design/Producer.java
@@ -0,0 +1,10 @@
+package system_design;
+
+public class Producer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, I am Producer");
+	}
+
+}
